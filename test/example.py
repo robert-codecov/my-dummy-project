6043f7bb9c07dd9cb1a4eedf3ece46f9907ce842
Robert Kotcher <robertkotcher@Roberts-MBP.fios-router.home>
2020-09-15 15:57:27 -0400

    Add sum function

--- test/example.py
+++ test/example.py
@@ -1,8 +1,14 @@
 import unittest
 
+def add(a, b):
+    if a > b:
+        return a + b
+    else:
+        return a + b
+
 class TestMethods(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(1, 1)
+        self.assertEqual(add(2, 2), 4)
 
 if __name__ == '__main__':
     unittest.main()
