097eeb4bfb92e2b2026850e72b503b8288c3a102
Robert Kotcher <robertkotcher@Roberts-MacBook-Pro.local>
2020-11-13 10:39:05 -0500

    Add a few uncovered functions to watch coverage drop

--- test/example.py
+++ test/example.py
@@ -1,6 +1,42 @@
 import unittest
 
+def div(a, b):
+    if a == None:
+        raise Exception("a must be defined")
+
+    if b == None:
+        raise Exception("b must be defined")
+    
+    if b == 0:
+        return None
+    else:
+        return a / b
+
+def mult(a, b):
+    if a == None:
+        raise Exception("a must be defined")
+
+    if b == None:
+        raise Exception("b must be defined")
+
+    return a * b
+
+def sub(a, b):
+    if a == None:
+        raise Exception("a must be defined")
+
+    if b == None:
+        raise Exception("b must be defined")
+
+    return a - b
+
 def add(a, b):
+    if a == None:
+        raise Exception("a must be defined")
+
+    if b == None:
+        raise Exception("b must be defined")
+
     if a > b:
         return a + b
     else:
