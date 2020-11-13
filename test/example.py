552122989253fa7a71ab97ad5ea4a2cefadcf4f5
Robert Kotcher <robertkotcher@Roberts-MacBook-Pro.local>
2020-11-13 10:53:03 -0500

    Add test for subtract function

--- test/example.py
+++ test/example.py
@@ -46,5 +46,8 @@ class TestMethods(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 2), 4)
 
+    def test_sub(self):
+        self.assertEqual(sub(10, 5), 5)
+
 if __name__ == '__main__':
     unittest.main()
